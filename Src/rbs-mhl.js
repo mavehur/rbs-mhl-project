914f62897ef3075c18f14356e37ed185bd2cf47c
Mave Hur <wlahd10@gmail.com>
2024-05-05 20:26:22 -0700

    first commit

---
+++ Src/rbs-mhl.js
@@ -0,0 +1,30 @@
+new DataTable('#myTable', {
+    initComplete: function () {
+        this.api()
+            .columns()
+            .every(function () {
+                let column = this;
+
+                // Create select element
+                let select = document.createElement('select');
+                select.add(new Option(''));
+                column.footer().replaceChildren(select);
+
+                // Apply listener for user change in value
+                select.addEventListener('change', function () {
+                    column
+                        .search(select.value, {exact: true})
+                        .draw();
+                });
+
+                // Add list of options
+                column
+                    .data()
+                    .unique()
+                    .sort()
+                    .each(function (d, j) {
+                        select.add(new Option(d));
+                    });
+            });
+    }
+});
