bb527374777131ebb59c1d63a483aced427ea6fa
Mave Hur <wlahd10@gmail.com>
2024-05-11 19:27:28 -0700

    merge Parth's branch to render data in search pane

--- Src/rbs-mhl.js
+++ Src/rbs-mhl.js
@@ -1,35 +1,94 @@
-new DataTable('#myTable', {
-    //filtering section
-    layout: {
-        top1: {
-            searchPanes: {
-                cascadePanes: true, //panes to be filtered based on the values selected in the other panes.
-                columns: [0, 6, 9, 4, 7, 8],
-                viewCount: false,
-                orderable: false,
-                collapse: false,
-                dtOpts:{
-                    select:{
-                        style: 'multi'
+$(document).ready(function () {
+    fetch('./assets\\data\\books.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        $('#myTable').DataTable({
+            data: data,
+            dom: 'PBfrtip',
+            columns: [
+               {
+                    data: 'Topic', render: function (data, type, row) {
+                        if (Array.isArray(data) && data.length > 0) {
+                            return data.join(', ');
+                        } else {
+                            return '';
+                        }
+                    }
+                },
+                {
+                    data: 'Title', render: function (data, type, row) {
+                        return data ? data + ' ' + '<a href="' + row['Link to GoodReads'] + '" target="_blank">' + '<img src="./assets\\images\\greads_icon.png" width="20" height="20">' : '';
+                    }
+                },
+                { data: 'Author' },
+                {
+                    data: 'Age Range', render: function (data, type, row) {
+                        if (Array.isArray(data) && data.length > 0) {
+                            return data[0] + ' - ' + data[data.length - 1];
+                        } else {
+                            return '';
+                        }
+                    }
+                },
+                {
+                    data: 'Language', render: function (data, type, row) {
+                        if (Array.isArray(data) && data.length > 0) {
+                            return data.join(', ');
+                        } else {
+                            return '';
+                        }
+                    }
+                },
+                { data: 'Number of Times Recommended' },
+                {
+                    data: 'Who Recommended', render: function (data, type, row) {
+                        if (Array.isArray(data) && data.length > 0) {
+                            return data.join(', ');
+                        } else {
+                            return '';
+                        }
                     }
                 }
-            }
-        },
-        topStart: {
-            //TODO: add 'pdf'. currently it's not working
-            buttons: ['copy', 'csv', 'excel', 'print']
-        }
-    },
-    //data section
-    columnDefs: [
-        {
-            searchPanes: {
-                show: true,
+            ],
+            //filtering section
+            layout: {
+                top1: {
+                    searchPanes: {
+                        columns: [0, 2, 3, 4, 5, 6],
+                    }
+                },
+                topStart: {
+                    //TODO: add 'pdf'. currently it's not working
+                    buttons: ['copy', 'csv', 'excel', 'print']
+                }
             },
-            targets: [0, 4, 6, 7, 8, 9] //search category
-        }
-    ],
-    stateSave: true,
-
+            //data section
+            columnDefs: [
+                {
+                    searchPanes: {
+                        cascadePanes: true, //panes to be filtered based on the values selected in the other panes.
+                        show: true,
+                        viewCount: false,
+                        orderable: false,
+                        collapse: false,
+                        dtOpts:{
+                            select:{
+                                style: 'multi'
+                            }
+                        }
+                    },
+                    targets: [0, 2, 3, 4, 5, 6] //search category
+                }
+            ],
+            stateSave: true,
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 });
-
